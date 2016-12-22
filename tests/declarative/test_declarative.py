1a76d83139a87aa44df4aa1050a5d7fa7cfce805
Michael Williamson <mike@zwobble.org>
2016-12-22 12:00:21 +0000

    Add tests for declarative single()

--- tests/declarative/test_declarative.py
+++ tests/declarative/test_declarative.py
@@ -1,7 +1,7 @@
 import attr
 from hamcrest import assert_that, equal_to
 
-from graphjoiner.declarative import executor, field, many, RootType, ObjectType, extract
+from graphjoiner.declarative import executor, field, single, many, RootType, ObjectType, extract
 from graphql import GraphQLString
 
 
@@ -19,6 +19,38 @@ class StaticDataObjectType(ObjectType):
             )
             for record in cls.__records__
         ]
+
+
+def test_single_relationship_is_resolved_to_null_if_there_are_no_matching_results():
+    class Author(StaticDataObjectType):
+        __records__ = []
+
+        name = field(type=GraphQLString)
+
+    class Root(RootType):
+        author = single(Author)
+
+    result = executor(Root)("{ author { name } }")
+    assert_that(result, equal_to({
+        "author": None,
+    }))
+
+
+def test_single_relationship_is_resolved_to_object_if_there_is_exactly_one_matching_result():
+    AuthorRecord = attr.make_class("AuthorRecord", ["name"])
+
+    class Author(StaticDataObjectType):
+        __records__ = [AuthorRecord("PG Wodehouse")]
+
+        name = field(type=GraphQLString)
+
+    class Root(RootType):
+        author = single(Author)
+
+    result = executor(Root)("{ author { name } }")
+    assert_that(result, equal_to({
+        "author": {"name": "PG Wodehouse"},
+    }))
 
 
 def test_can_extract_fields_from_relationships():
